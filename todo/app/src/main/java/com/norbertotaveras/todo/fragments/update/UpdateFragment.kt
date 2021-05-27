f32e48bd27b36cb64f9ee2476933fa3643e096f6
Norberto Taveras <nm.taveras@gmail.com>
2021-05-27 11:12:48 -0400

    Update Fragment
    ADDED - The update fragment and its own custom menu.

--- todo/app/src/main/java/com/norbertotaveras/todo/fragments/update/UpdateFragment.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/fragments/update/UpdateFragment.kt
@@ -1,19 +1,21 @@
 package com.norbertotaveras.todo.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.norbertotaveras.todo.R
 
 class UpdateFragment : Fragment() {
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
