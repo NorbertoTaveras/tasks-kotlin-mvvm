e25d3b611da57b0604105efb97292ac402d0412b
Norberto Taveras <nm.taveras@gmail.com>
2021-05-27 13:14:47 -0400

    Todo Database
    ADDED - The initial implementation of the todo database.

---
+++ todo/app/src/main/java/com/norbertotaveras/todo/room/database/TodoDatabase.kt
@@ -0,0 +1,36 @@
+package com.norbertotaveras.todo.room.database
+
+import android.content.Context
+import androidx.room.*
+import com.norbertotaveras.todo.room.converters.PriorityConverter
+import com.norbertotaveras.todo.room.daos.TodoDao
+import com.norbertotaveras.todo.room.entities.TodoEntity
+
+@Database(entities = [TodoEntity::class], version = 1, exportSchema = false)
+@TypeConverters(PriorityConverter::class)
+abstract class TodoDatabase: RoomDatabase() {
+
+    abstract fun todoDao(): TodoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TodoDatabase? = null
+
+        fun getInstance(context: Context): TodoDatabase {
+            val temp = INSTANCE
+
+            if (temp != null)
+                return temp
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    TodoDatabase::class.java,
+                    "todo.db")
+                    .build()
+                INSTANCE = instance
+                return INSTANCE as TodoDatabase
+            }
+        }
+    }
+}
