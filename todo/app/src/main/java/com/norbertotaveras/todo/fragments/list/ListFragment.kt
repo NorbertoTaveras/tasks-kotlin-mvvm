849a9c128e84dd94990d48bc1b470c015f9bf6ab
Norberto Taveras <nm.taveras@gmail.com>
2021-05-27 20:52:31 -0400

    Adapter Assignment
    ADDED - Assign the recycler view adapter to display that appropiately.

--- todo/app/src/main/java/com/norbertotaveras/todo/fragments/list/ListFragment.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/fragments/list/ListFragment.kt
@@ -5,14 +5,24 @@ import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.norbertotaveras.todo.R
+import com.norbertotaveras.todo.adapters.TodosAdapter
+import com.norbertotaveras.todo.viewmodels.TodoViewModel
 
 class ListFragment : Fragment(), View.OnClickListener {
 
     private lateinit var button: FloatingActionButton
     private lateinit var layout: ConstraintLayout
+    private lateinit var recyclerview: RecyclerView
+
+    private val adapter: TodosAdapter by lazy { TodosAdapter() }
+    private val todoViewModel: TodoViewModel by viewModels()
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.list_fragment_menu, menu)
@@ -26,6 +36,14 @@ class ListFragment : Fragment(), View.OnClickListener {
 
         button = view.findViewById(R.id.floatingActionButton)
         layout = view.findViewById(R.id.listLayout)
+        recyclerview = view.findViewById(R.id.recyclerView)
+
+        recyclerview.adapter = adapter
+        recyclerview.layoutManager = LinearLayoutManager(requireActivity())
+
+        todoViewModel.todos.observe(viewLifecycleOwner, Observer { todos ->
+            adapter.set(todos)
+        })
 
         button.setOnClickListener(this)
         layout.setOnClickListener(this)
@@ -37,7 +55,6 @@ class ListFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.floatingActionButton -> findNavController().navigate(R.id.action_listFragment_to_addFragment)
-            R.id.listLayout -> findNavController().navigate(R.id.action_listFragment_to_updateFragment)
             else -> Log.e("Error", "Unexpected Case")
         }
     }
