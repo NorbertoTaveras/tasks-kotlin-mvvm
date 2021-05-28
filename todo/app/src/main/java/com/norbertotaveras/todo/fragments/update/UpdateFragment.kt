2f7f03ffd463c6ca092227b114cc84664c36f1e1
Norberto Taveras <nm.taveras@gmail.com>
2021-05-27 20:54:03 -0400

    Update Fragment Data Retrieval & Parsing
    Added - The retrieval of data that was passed forward through safe args from the main screen.
    
    Added - A method to parse/classify priorities from the todo looked up.

--- todo/app/src/main/java/com/norbertotaveras/todo/fragments/update/UpdateFragment.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/fragments/update/UpdateFragment.kt
@@ -2,10 +2,23 @@ package com.norbertotaveras.todo.fragments.update
 
 import android.os.Bundle
 import android.view.*
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.norbertotaveras.todo.R
+import com.norbertotaveras.todo.models.Priority
+import com.norbertotaveras.todo.viewmodels.SharedViewModel
 
 class UpdateFragment : Fragment() {
+
+    private lateinit var title: EditText
+    private lateinit var description: EditText
+    private lateinit var spinner: Spinner
+
+    private val args by navArgs<UpdateFragmentArgs>()
+    private val sharedViewModel: SharedViewModel by viewModels()
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.update_fragment_menu, menu)
@@ -15,8 +28,25 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_update, container, false)
+
+        title = view.findViewById(R.id.current_title_et)
+        description = view.findViewById(R.id.current_description_et)
+        spinner = view.findViewById(R.id.current_priority_spinner)
+
+        title.setText(args.currentTodo.title)
+        description.setText(args.currentTodo.description)
+        spinner.setSelection(parse(args.currentTodo.priority))
+        spinner.onItemSelectedListener = sharedViewModel.listener
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        return view
     }
 
+    private fun parse(priority: Priority): Int {
+        return when (priority) {
+            Priority.HIGH -> 0
+            Priority.MEDIUM -> 1
+            Priority.LOW -> 2
+        }
+    }
 }
