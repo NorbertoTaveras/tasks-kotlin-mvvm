14bcc1d6fef456f6e93e9ca3cbb9fb8f230c7622
Norberto Taveras <nm.taveras@gmail.com>
2021-05-27 20:54:42 -0400

    Parcelable
    Added - Parcelable implementation into the todo entity.

--- todo/app/src/main/java/com/norbertotaveras/todo/room/entities/TodoEntity.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/room/entities/TodoEntity.kt
@@ -1,14 +1,17 @@
 package com.norbertotaveras.todo.room.entities
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.norbertotaveras.todo.models.Priority
+import kotlinx.android.parcel.Parcelize
 
 @Entity(tableName = "table_todos")
+@Parcelize
 data class TodoEntity(
     @PrimaryKey(autoGenerate = true)
     var id: Int,
     var title: String,
     var priority: Priority,
-    var description: String) {
+    var description: String): Parcelable {
 }
