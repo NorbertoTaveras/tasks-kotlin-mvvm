4ad24ce742aac1727da65140580952ed0d1263c2
Norberto Taveras <nm.taveras@gmail.com>
2021-05-27 20:56:42 -0400

    SharedVIewModel
    Added - A brief implementation of the shared view model.
    Added - A method to verify that the user input is valid.
    Added - A method to parse and lookup a priority based on a given string passed in.
    Added - Implementation of assigning a unique color to the spinner textview after a priority is selected.

---
+++ todo/app/src/main/java/com/norbertotaveras/todo/viewmodels/SharedViewModel.kt
@@ -0,0 +1,43 @@
+package com.norbertotaveras.todo.viewmodels
+
+import android.app.Application
+import android.view.View
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import com.norbertotaveras.todo.R
+import com.norbertotaveras.todo.models.Priority
+
+class SharedViewModel(application: Application): AndroidViewModel(application) {
+
+    val listener: AdapterView.OnItemSelectedListener = object :
+        AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?,
+                                    view: View?,
+                                    position: Int,
+                                    id: Long) {
+            when (position) {
+                0 -> {(parent?.getChildAt(0) as? TextView)?.setTextColor(ContextCompat.getColor(application, R.color.red))}
+                1 -> {(parent?.getChildAt(0) as? TextView)?.setTextColor(ContextCompat.getColor(application, R.color.yellow))}
+                2 -> {(parent?.getChildAt(0) as? TextView)?.setTextColor(ContextCompat.getColor(application, R.color.green))}
+            }
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+    }
+
+    fun verify(title: String, description: String): Boolean {
+        return !(title.isEmpty() || description.isEmpty())
+    }
+
+    fun lookup(priority: String): Priority {
+        return when (priority) {
+            "High" -> Priority.HIGH
+            "Medium" -> Priority.MEDIUM
+            "Low" -> Priority.LOW
+            else -> Priority.LOW
+        }
+    }
+}
