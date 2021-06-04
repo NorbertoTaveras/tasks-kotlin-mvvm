c16f0449ff17b57a47e198a1b7b86c6a867ca5d5
Norberto Taveras <nm.taveras@gmail.com>
2021-06-04 15:52:22 -0400

    Input & UI Validation
    Added - Implementation of required fields validation for both the title and description.
    Added - Implementation of UI validation for both required fields to display a error text message accordingly.
    Added - Hide the keyboard once a task is inserted into the database successfully.
    Added - A snackbar to be displayed when the required fields are missing.
    Added - Adapter creation from resource of strings.
    Added - Observer/collect  when navigating back with result with the event system implemented by setting the fragment result with specific keys for request and result before navigating back to the list fragment.

--- todo/app/src/main/java/com/norbertotaveras/todo/fragments/add/AddFragment.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/fragments/add/AddFragment.kt
@@ -4,26 +4,36 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.*
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.norbertotaveras.todo.R
+import com.norbertotaveras.todo.databinding.FragmentAddBinding
 import com.norbertotaveras.todo.models.Priority
 import com.norbertotaveras.todo.room.entities.TodoEntity
+import com.norbertotaveras.todo.utils.createSnackBar
+import com.norbertotaveras.todo.utils.hideKeyboard
 import com.norbertotaveras.todo.viewmodels.SharedViewModel
 import com.norbertotaveras.todo.viewmodels.TodoViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class AddFragment : Fragment() {
-
-    private lateinit var title: EditText
-    private lateinit var priorities: Spinner
-    private lateinit var description: EditText
 
     private val todoViewModel: TodoViewModel by viewModels()
     private val sharedViewModel: SharedViewModel by viewModels()
+
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
@@ -32,15 +42,29 @@ class AddFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_add, container, false)
+    ): View {
+        _binding = FragmentAddBinding.inflate(layoutInflater, container, false)
         setHasOptionsMenu(true)
 
-        title = view.findViewById(R.id.title_et)
-        description = view.findViewById(R.id.description_et)
-        priorities = view.findViewById(R.id.priority_spinner)
-        priorities.onItemSelectedListener = sharedViewModel.listener
-        return view
+        val adapter = ArrayAdapter.createFromResource(requireContext(),
+            R.array.priorities,
+            android.R.layout.simple_spinner_dropdown_item)
+
+        binding.prioritySpinner.adapter = adapter
+        binding.prioritySpinner.onItemSelectedListener = sharedViewModel.tiper
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            todoViewModel.addTaskEvent.collect { event ->
+                when (event) {
+                    is TodoViewModel.AddTaskEvent.NavigateBackWithAddResult -> {
+                        setFragmentResult("add_request", bundleOf("add_result" to event.result))
+                        findNavController().navigate(R.id.action_addFragment_to_listFragment)
+                    }
+                }
+            }
+        }
+
+        return binding.root
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -52,21 +76,27 @@ class AddFragment : Fragment() {
     }
 
     private fun insertTodo() {
-        val title = title.text.toString()
-        val priority = priorities.selectedItem.toString()
-        val description = description.text.toString()
-
+        val title = binding.titleEt.text.toString()
+        val priority = binding.prioritySpinner.selectedItem.toString()
+        val description = binding.descriptionEt.text.toString()
         val validation = sharedViewModel.verify(title, description)
+
+        when (title.isEmpty()) {
+            true -> {binding.titleEtLayout.error = getString(R.string.error_title_field_required)}
+            false -> {binding.titleEtLayout.error = null}
+        }
+
+        when (description.isEmpty()) {
+            true -> {binding.descriptionEtLayout.error = getString(R.string.error_description_field_required)}
+            false -> {binding.descriptionEtLayout.error = null}
+        }
 
         if (validation) {
             val todo = TodoEntity(0, title, sharedViewModel.lookup(priority), description, false)
             todoViewModel.insert(todo)
-            Toast.makeText(requireContext(), getString(R.string.successfully_added), Toast.LENGTH_SHORT).show()
-            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+            hideKeyboard(requireActivity())
         } else {
-            Toast.makeText(requireContext(), getString(R.string.required_fields), Toast.LENGTH_SHORT).show()
+            createSnackBar(requireView(), R.string.required_fields, Snackbar.LENGTH_LONG)
         }
     }
-
-
 }
