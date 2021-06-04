67ec999f81e44027cdfae943f2a3dc8d6aadeecd
Norberto Taveras <nm.taveras@gmail.com>
2021-06-04 16:34:53 -0400

    Modified Queries
    Added - Query to delete all tasks marked as completed by the user from the database.
    Added - Query to retrieved all tasks sorted by a specific priority from the database, while also keeping track of if they've been marked as completed or not by the user.
    Added - Method to retrieve all tasks based on the sort order and completed status, while also conforming to the Flow instead of Live Data.

--- todo/app/src/main/java/com/norbertotaveras/todo/room/daos/TodoDao.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/room/daos/TodoDao.kt
@@ -2,14 +2,24 @@ package com.norbertotaveras.todo.room.daos
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.norbertotaveras.todo.models.Priority
 import com.norbertotaveras.todo.room.entities.TodoEntity
+import com.norbertotaveras.todo.utils.SortOrder
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoDao {
 
     @Query("SELECT * FROM table_todos ORDER BY id ASC")
     fun getAll(): LiveData<MutableList<TodoEntity>>
+
+    fun getAllFlow(sortOrder: SortOrder, hideCompleted: Boolean): Flow<MutableList<TodoEntity>> =
+        when (sortOrder) {
+            SortOrder.BY_DATE -> date(hideCompleted)
+            SortOrder.BY_NAME -> title(hideCompleted)
+            SortOrder.BY_HIGH -> high(hideCompleted)
+            SortOrder.BY_MEDIUM -> medium(hideCompleted)
+            SortOrder.BY_LOW -> low(hideCompleted)
+        }
 
     @Query("DELETE FROM table_todos")
     suspend fun deleteAll()
@@ -29,12 +39,21 @@ interface TodoDao {
     @Query("SELECT * FROM table_todos WHERE title LIKE :query")
     fun search(query: String): LiveData<MutableList<TodoEntity>>
 
-    @Query("SELECT * FROM table_todos ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
-    fun low(): LiveData<MutableList<TodoEntity>>
+    @Query("SELECT * FROM table_todos WHERE (completed != :hideCompleted or completed = 0) ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+    fun low(hideCompleted: Boolean): Flow<MutableList<TodoEntity>>
 
-    @Query("SELECT * FROM table_todos ORDER BY CASE WHEN priority LIKE 'M%' THEN 1 WHEN priority LIKE 'L%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
-    fun medium(): LiveData<MutableList<TodoEntity>>
+    @Query("SELECT * FROM table_todos WHERE (completed != :hideCompleted or completed = 0) ORDER BY CASE WHEN priority LIKE 'M%' THEN 1 WHEN priority LIKE 'L%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+    fun medium(hideCompleted: Boolean): Flow<MutableList<TodoEntity>>
 
-    @Query("SELECT * FROM table_todos ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
-    fun high(): LiveData<MutableList<TodoEntity>>
+    @Query("SELECT * FROM table_todos WHERE (completed != :hideCompleted or completed = 0) ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
+    fun high(hideCompleted: Boolean): Flow<MutableList<TodoEntity>>
+
+    @Query("SELECT * FROM table_todos WHERE (completed != :hideCompleted or completed = 0) ORDER BY title")
+    fun title(hideCompleted: Boolean): Flow<MutableList<TodoEntity>>
+
+    @Query("SELECT * FROM table_todos WHERE (completed != :hideCompleted or completed = 0) ORDER BY created")
+    fun date(hideCompleted: Boolean): Flow<MutableList<TodoEntity>>
+
+    @Query("DELETE FROM table_todos WHERE completed = 1")
+    suspend fun deleteCompletedTasks()
 }
