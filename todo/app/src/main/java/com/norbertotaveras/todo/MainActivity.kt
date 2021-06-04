f0f85b59fa5f7278f46f70e73faa23efe5448c21
Norberto Taveras <nm.taveras@gmail.com>
2021-06-04 16:30:52 -0400

    Result Codes
    Added - Constant values representing the result codes for delete, update and add tasks.

--- todo/app/src/main/java/com/norbertotaveras/todo/MainActivity.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.norbertotaveras.todo
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,3 +23,7 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
+
+const val DELETE_TASK_RESULT_OK = Activity.RESULT_FIRST_USER
+const val UPDATE_TASK_RESULT_OK = Activity.RESULT_FIRST_USER + 1
+const val ADD_TASK_RESULT_OK = Activity.RESULT_FIRST_USER + 2
