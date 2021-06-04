680ca8f130277c5afc54eb8a80eacf6a93011c75
Norberto Taveras <nm.taveras@gmail.com>
2021-06-04 16:32:23 -0400

    Coroutine Delete Completed Tasks
    Added - Method that calls the delete completed tasks implementation within the todo dao.

--- todo/app/src/main/java/com/norbertotaveras/todo/repositories/TodoRepository.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/repositories/TodoRepository.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.LiveData
 import com.norbertotaveras.todo.models.Priority
 import com.norbertotaveras.todo.room.daos.TodoDao
 import com.norbertotaveras.todo.room.entities.TodoEntity
+import kotlinx.coroutines.flow.Flow
 
 class TodoRepository(private val todoDao: TodoDao) {
     val getAll: LiveData<MutableList<TodoEntity>> = todoDao.getAll()
-    val low: LiveData<MutableList<TodoEntity>> = todoDao.low()
-    val medium: LiveData<MutableList<TodoEntity>> = todoDao.medium()
-    val high: LiveData<MutableList<TodoEntity>> = todoDao.high()
+    //val low: LiveData<MutableList<TodoEntity>> = todoDao.low()
+    //val medium: LiveData<MutableList<TodoEntity>> = todoDao.medium()
+    //val high: LiveData<MutableList<TodoEntity>> = todoDao.high(false)
+    //val title: LiveData<MutableList<TodoEntity>> = todoDao.title(hideCompleted = false)
 
     suspend fun insert(todo: TodoEntity) {
         todoDao.insert(todo)
@@ -25,6 +27,10 @@ class TodoRepository(private val todoDao: TodoDao) {
 
     suspend fun deleteAll() {
         todoDao.deleteAll()
+    }
+
+    suspend fun deleteCompletedTasks() {
+        todoDao.deleteCompletedTasks()
     }
 
     fun search(query: String): LiveData<MutableList<TodoEntity>> {
