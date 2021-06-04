c7ff33dd849e4cff394924263c49bfed3a60f55b
Norberto Taveras <nm.taveras@gmail.com>
2021-06-04 16:47:01 -0400

    Custom Create Snackbar Method
    Added - Methods to create and show snackbar based on the parameters passed in as needed.

--- todo/app/src/main/java/com/norbertotaveras/todo/utils/Utils.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/utils/Utils.kt
@@ -2,10 +2,17 @@ package com.norbertotaveras.todo.utils
 
 import android.app.Activity
 import android.content.Context
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+import com.norbertotaveras.todo.R
+import com.norbertotaveras.todo.fragments.update.UpdateFragmentArgs
+import com.norbertotaveras.todo.room.entities.TodoEntity
 
 fun hideKeyboard(activity: Activity) {
     val manager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -22,4 +29,21 @@ fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observ
             removeObserver(this)
         }
     })
+}
+
+fun createSnackBar(view: View, @StringRes message: Int, duration: Int) {
+    val resources = view.resources
+    val snackBar = Snackbar.make(view, resources.getString(message), duration)
+    snackBar.show()
+}
+
+fun createSnackBar(view: View, @StringRes message: Int, args: Any, duration: Int) {
+    val resources = view.resources
+    val snackBar = Snackbar.make(view, resources.getString(message, args), duration)
+    snackBar.show()
+}
+
+fun createSnackBar(view: View,  message: String, duration: Int) {
+    val snackBar = Snackbar.make(view, message, duration)
+    snackBar.show()
 }
