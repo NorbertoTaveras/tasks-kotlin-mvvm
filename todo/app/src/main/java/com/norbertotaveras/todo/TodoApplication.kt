de5acecb908c61fb8871c57f96455f57281d2cb3
Norberto Taveras <nm.taveras@gmail.com>
2021-06-04 17:21:32 -0400

    Event System
    Added - Implementation of a generic event system to be used across fragment when sending navigating back with results to the list fragment of tasks/todos.

---
+++ todo/app/src/main/java/com/norbertotaveras/todo/TodoApplication.kt
@@ -0,0 +1,8 @@
+package com.norbertotaveras.todo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TodoApplication : Application() {
+}
--- todo/app/src/main/java/com/norbertotaveras/todo/viewmodels/TodoViewModel.kt
+++ todo/app/src/main/java/com/norbertotaveras/todo/viewmodels/TodoViewModel.kt
@@ -1,49 +1,82 @@
 package com.norbertotaveras.todo.viewmodels
 
 import android.app.Application
+import androidx.annotation.StringRes
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.norbertotaveras.todo.models.Todo
+import com.norbertotaveras.todo.ADD_TASK_RESULT_OK
+import com.norbertotaveras.todo.DELETE_TASK_RESULT_OK
+import com.norbertotaveras.todo.R
+import com.norbertotaveras.todo.UPDATE_TASK_RESULT_OK
 import com.norbertotaveras.todo.repositories.TodoRepository
 import com.norbertotaveras.todo.room.database.TodoDatabase
 import com.norbertotaveras.todo.room.entities.TodoEntity
+import com.norbertotaveras.todo.utils.PreferencesManager
+import com.norbertotaveras.todo.utils.SortOrder
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class TodoViewModel(application: Application): AndroidViewModel(application) {
+@HiltViewModel
+class TodoViewModel @Inject constructor(application: Application, private val preferencesManager: PreferencesManager): AndroidViewModel(application) {
 
     private val todoDao = TodoDatabase.getInstance(application).todoDao()
     private val repository: TodoRepository
-    val todos: LiveData<MutableList<TodoEntity>>
-    val low: LiveData<MutableList<TodoEntity>>
-    val medium: LiveData<MutableList<TodoEntity>>
-    val high: LiveData<MutableList<TodoEntity>>
+
+    private val updateDeleteTaskEventChannel = Channel<UpdateDeleteTaskEvent>()
+    private val tasksEventChannel = Channel<TasksEvent>()
+    private val addTaskEventChannel = Channel<AddTaskEvent>()
+
+    val tasksEvent = tasksEventChannel.receiveAsFlow()
+    val updateDeleteTaskEvent = updateDeleteTaskEventChannel.receiveAsFlow()
+    val addTaskEvent = addTaskEventChannel.receiveAsFlow()
+    val preferencesFlow = preferencesManager.preferencesFlow
+
+    @ExperimentalCoroutinesApi
+    private val tasksFlow = combine(
+        preferencesFlow
+    ) { filterPreferences ->
+        filterPreferences
+    }.flatMapLatest { (filterPreferences) ->
+        todoDao.getAllFlow(filterPreferences.sortOrder, filterPreferences.hideCompleted)
+    }
+
+    @ExperimentalCoroutinesApi
+    val tasks = tasksFlow.asLiveData()
 
     init {
         repository = TodoRepository(todoDao)
-        todos = repository.getAll
-        low = repository.low
-        medium = repository.medium
-        high = repository.high
     }
 
     fun insert(todo: TodoEntity) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.insert(todo)
+            addTaskEventChannel.send(AddTaskEvent.NavigateBackWithAddResult(ADD_TASK_RESULT_OK))
         }
     }
 
     fun update(todo: TodoEntity) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.update(todo)
+            updateDeleteTaskEventChannel.send(
+                UpdateDeleteTaskEvent.NavigateBackWithUpdateResult(UPDATE_TASK_RESULT_OK))
         }
     }
 
     fun delete(todo: TodoEntity) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.delete(todo)
+            updateDeleteTaskEventChannel.send(
+                UpdateDeleteTaskEvent.NavigateBackWithDeleteResult(DELETE_TASK_RESULT_OK))
         }
     }
 
@@ -53,18 +86,29 @@ class TodoViewModel(application: Application): AndroidViewModel(application) {
         }
     }
 
+    fun deleteCompletedTasks() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteCompletedTasks()
+        }
+    }
+
     fun search(query: String): LiveData<MutableList<TodoEntity>> {
         return repository.search(query)
     }
 
-    fun completed(todo: TodoEntity, complete: Boolean) {
-        todo.completed = complete
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.update(todo)
+    fun onTaskSelected(todo: TodoEntity) {
+    }
+
+    fun onSortOrderSelected(sortOrder: SortOrder) {
+        viewModelScope.launch {
+            preferencesManager.updateSortOrder(sortOrder)
         }
     }
 
-    fun onTaskSelected(todo: TodoEntity) {
+    fun onHideCompletedClick(hideCompleted: Boolean) {
+        viewModelScope.launch {
+            preferencesManager.updateHideCompleted(hideCompleted)
+        }
     }
 
     fun onTaskCheckedChanged(todo: TodoEntity, isChecked: Boolean) {
@@ -73,4 +117,50 @@ class TodoViewModel(application: Application): AndroidViewModel(application) {
         }
     }
 
+    fun onUpdateDeleteResult(result: Int) {
+        when (result) {
+            DELETE_TASK_RESULT_OK -> showDeleteMessage(R.string.task_successfully_removed)
+            UPDATE_TASK_RESULT_OK -> showUpdateMessage(R.string.successfully_updated)
+        }
+    }
+
+    fun onAddResult(result: Int) {
+        when(result) {
+            ADD_TASK_RESULT_OK -> showAddMessage(R.string.successfully_added)
+        }
+    }
+
+    private fun showDeleteMessage(res: Int) {
+        viewModelScope.launch {
+            tasksEventChannel.send(TasksEvent.ShowTaskDeleteMessage(res))
+        }
+    }
+
+    private fun showUpdateMessage(res: Int) {
+        viewModelScope.launch {
+            tasksEventChannel.send(TasksEvent.ShowTaskUpdateMessage(res))
+        }
+    }
+
+    private fun showAddMessage(res: Int) {
+        viewModelScope.launch {
+            tasksEventChannel.send(TasksEvent.ShowAddMessage(res))
+        }
+    }
+
+    sealed class AddTaskEvent {
+        data class NavigateBackWithAddResult(val result: Int): AddTaskEvent()
+    }
+
+    sealed class UpdateDeleteTaskEvent {
+        data class NavigateBackWithDeleteResult(val result: Int) : UpdateDeleteTaskEvent()
+        data class NavigateBackWithUpdateResult(val result: Int) : UpdateDeleteTaskEvent()
+    }
+
+    sealed class TasksEvent {
+        data class ShowTaskDeleteMessage(@StringRes val res: Int): TasksEvent()
+        data class ShowTaskUpdateMessage(@StringRes val res: Int): TasksEvent()
+        data class ShowAddMessage(@StringRes val res: Int): TasksEvent()
+    }
 }
+
